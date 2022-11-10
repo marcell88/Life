c3109c8648e4e9d7b2a6c0d4d0c386c83749f671
Markell Davydov <marcell.davydov@gmail.com>
2022-11-10 13:37:18 +0300

    Game works

--- script.js
+++ script.js
@@ -6,150 +6,85 @@ const createCell = () => {
 }
 
 //Количество живых соседей
-const neighbours = (array, x, y, x_length, y_length) => {
-    if (x==0 && y==0) {
-        return (array[x][y_length-1] + 
-            array[x+1][y_length-1] + 
-            array[x+1][y] + 
-            array[x+1][y+1] + 
-            array[x][y+1] + 
-            array[x_length-1][y+1] + 
-            array[x_length-1][y] + 
-            array[x_length-1][y_length-1]);
-    } else if (x==0 && y==y_length-1) {
-        return (array[x][y-1] + 
-            array[x+1][y-1] + 
-            array[x+1][y] + 
-            array[x+1][0] + 
-            array[x][0] + 
-            array[x_length-1][0] + 
-            array[x_length-1][y] + 
-            array[x_length-1][y-1]);            
-    } else if (x==x_length-1 && y==0) {
-        return (array[x][y_length-1] + 
-            array[0][y_length-1] + 
-            array[0][y] + 
-            array[0][y+1] + 
-            array[x][y+1] + 
-            array[x-1][y+1] + 
-            array[x-1][y] + 
-            array[x-1][y_length-1]);
-    } else if (x==x_length-1 && y==y_length-1) {
-        return (array[x][y-1] + 
-            array[0][y-1] + 
-            array[0][y] + 
-            array[0][0] + 
-            array[x][0] + 
-            array[x-1][0] + 
-            array[x-1][y] + 
-            array[x-1][y-1]);
-    } else if (x==0) {
-        return (array[x][y-1] + 
-            array[x+1][y-1] + 
-            array[x+1][y] + 
-            array[x+1][y+1] + 
-            array[x][y+1] + 
-            array[x_length-1][y+1] + 
-            array[x_length-1][y] + 
-            array[x_length-1][y-1]);       
-    } else if (y==0) {
-        return (array[x][y_length-1] + 
-            array[x+1][y_length-1] + 
-            array[x+1][y] + 
-            array[x+1][y+1] + 
-            array[x][y+1] + 
-            array[x-1][y+1] + 
-            array[x-1][y] + 
-            array[x-1][y_length-1]);     
-    } else if (y == y_length-1) {
-        return (array[x][y-1] + 
-            array[x+1][y-1] + 
-            array[x+1][y] + 
-            array[x+1][0] + 
-            array[x][0] + 
-            array[x-1][0] + 
-            array[x-1][y] + 
-            array[x-1][y-1]);   
-    } else if (x == x_length-1) {
-        return (array[x][y-1] + 
-            array[0][y-1] + 
-            array[0][y] + 
-            array[0][y+1] + 
-            array[x][y+1] + 
-            array[x-1][y+1] + 
-            array[x-1][y] + 
-            array[x-1][y-1]);
-    } else {
-        return (array[x][y-1] + 
-            array[x+1][y-1] + 
-            array[x+1][y] + 
-            array[x+1][y+1] + 
-            array[x][y+1] + 
-            array[x-1][y+1] + 
-            array[x-1][y] + 
-            array[x-1][y-1]);
-    }
+const neighbours = (array, x, y, columns, rows) => {
+    let x_plus1 = (x==columns-1) ? 0 : (x+1);
+    let x_minus1 = (x==0) ? (columns-1) : (x-1);
+    let y_plus1 = (y==rows-1) ? 0 : (y+1);
+    let y_minus1 = (y==0) ? (rows-1) : (y-1);
+
+    return (array[x][y_minus1] + 
+        array[x_plus1 ][y_minus1] + 
+        array[x_plus1 ][y] + 
+        array[x_plus1 ][y_plus1] + 
+        array[x][y_plus1] + 
+        array[x_minus1][y_plus1] + 
+        array[x_minus1][y] + 
+        array[x_minus1][y_minus1]);
 }
 
 //Игра жизни
 const gameOfLife = () => {
 
     //Переводим массив в двумерный
-    let x_length = Math.sqrt(checked.length);
-    let y_length = Math.sqrt(checked.length);
-    let cells = new Array(x_length);
-    let cells_next = new Array(x_length);
+    let rows = Math.sqrt(checked.length);
+    let columns = Math.sqrt(checked.length);
+    let cells = new Array(columns);
+    let cells_next = new Array(rows);
 
-    for (let i = 0; i<x_length; i++) {
-        cells[i] = new Array(y_length);
-        cells_next[i] = new Array(y_length);
+    for (let i = 0; i<columns; i++) {
+        cells[i] = new Array(rows);
+        cells_next[i] = new Array(rows);
     }
 
-    for (let y=0; y<y_length; y++) {
-        for (let x=0; x<x_length; x++) {
-            cells[x][y] = checked[y_length*y + x];
+    for (let y=0; y<rows; y++) {
+        for (let x=0; x<columns; x++) {
+            cells[x][y] = checked[columns*y + x];
             cells_next[x][y] = 0;
         }
     }
-    console.log(cells[1][1], cells[2][1], cells[3][1])
-    //запускаем игру - расчет след состояния
-    for(let i=0; i<100; i++) {
-        setTimeout(function () { }, 200);
-        console.log(cells[1][1], cells[2][1], cells[3][1])
 
-        for (let x=0; x<x_length; x++) {
-            for (let y=0; y<y_length; y++) {
-                let n = neighbours(cells, x, y, x_length, y_length);
-                if (cells[x][y]==0 && n==3) {
-                    cells_next[x][y] = 1;
-                } else if (cells[x][y]==1 && (n==3 || n==2)) {
-                    cells_next[x][y] = 1;
-                } else {
-                    cells_next[x][y] = 0;
-                }
+
+    //запускаем игру - расчет след состояния
+    for (let y=0; y<rows; y++) {
+        for (let x=0; x<columns; x++) {
+            let n = neighbours(cells, x, y, columns, rows);
+            if (cells[x][y]==0 && n==3) {
+                cells_next[x][y] = 1;
+                //console.log(n);
+            } else if (cells[x][y]==1 && (n==3 || n==2)) {
+                cells_next[x][y] = 1;
+               // console.log(n);
+            } else {
+                cells_next[x][y] = 0;
             }
         }
-
-        
-        //копируем и рисуем состояния
-        for (let y=0; y<y_length; y++) {
-            for (let x=0; x<x_length; x++) {
-                cells[x][y] = cells_next[x][y];
-                cells_next[x][y] = 0;
-
-                if (cells[x][y] == 1) {
-                    listOfCells[y_length*y + x].style.backgroundColor = "rgba(256, 0 , 0, 0.5)";     
-                    listOfCells[y_length*y + x].style.border = "1px solid rgba(256, 0 , 0, 1)";  
-                } else {
-                    listOfCells[y_length*y + x].style.backgroundColor = "transparent";   
-                    listOfCells[y_length*y + x].style.border = "1px solid rgba(256, 0 , 0, .1)";
-                }
+    }
+    
+    //копируем и рисуем состояния
+    for (let x=0; x<columns; x++) {
+            for (let y=0; y<rows; y++) {
+            cells[x][y] = cells_next[x][y];
+            cells_next[x][y] = 0;
+            if (cells[x][y] == 1) {
+                listOfCells[columns*y + x].style.backgroundColor = "rgba(256, 0 , 0, 0.5)";     
+                listOfCells[columns*y + x].style.border = "1px solid rgba(256, 0 , 0, 1)";  
+                checked[columns*y + x] = 1;
+            } else {
+                listOfCells[columns*y + x].style.backgroundColor = "transparent";   
+                listOfCells[columns*y + x].style.border = "1px solid rgba(256, 0 , 0, .1)";
+                checked[columns*y + x] = 0;
             }
         }
     }
 }
 
-
+const launch = () => {
+    if (k<max) {
+        k++;
+        gameOfLife();
+        setTimeout(launch, 150);
+    }   
+}
 
 /////// ХОД ПРОГРАММЫ ////////
 
@@ -181,4 +116,7 @@ for (let i=0; i<listOfCells.length; i++) {
 }
 
 var button = document.querySelector(".board__button");
-button.onclick = gameOfLife;
+
+let k = 0;
+let max = 1000;
+button.onclick = launch;
